1d885e1964c622b83d31f03432d6fb9c15d9b610
David Santucci <dsantucci@gnaritas.com>
2017-10-03 16:24:49 -0400

    Add test email form.

--- includes/admin-test-email.php
+++ includes/admin-test-email.php
@@ -1,1 +1,27 @@
-<h1>Test Email</h1>
+<?php include("_admin_page_open.php"); ?>
+<?php include("_admin_form_open.php"); ?>
+
+<?php $context = $_POST; ?>
+
+<table class="form-table">
+<tbody>
+    <tr>
+        <th scope="row"><label for="gnses_email">Email Address</label></th>
+        <td><input type="text" class="regular-text" id="gnses_email" name="email" value="{{email}}" /></td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="gnses_subject">Subject</label></th>
+        <td><input type="text" class="regular-text" id="gnses_subject" name="subject" value="{{subject}}" /></td>
+    </tr>
+    <tr>
+        <th scope="row"><label for="gnses_message">Message</label></th>
+        <td><textarea id="gnses_message" name="message" cols="80" rows="10">{{message}}</textarea></td>
+    </tr>
+</tbody>
+</table>
+
+<p class="submit">
+    <input type="submit" value="Send Test Email" class="button-primary" />
+</p>
+<?php include("_admin_form_close.php"); ?>
+<?php include("_admin_page_close.php"); ?>
