295d9f2a223d930a07dc8258d7d25a946f72444e
David Santucci <dsantucci@gnaritas.com>
2017-10-04 09:59:14 -0400

    Change test email form labels.

--- includes/admin-test-email.php
+++ includes/admin-test-email.php
@@ -6,15 +6,15 @@
 <table class="form-table">
 <tbody>
     <tr>
-        <th scope="row"><label for="gnses_email">Email Address</label></th>
+        <th scope="row"><label for="gnses_email">To:</label></th>
         <td><input type="text" class="regular-text" id="gnses_email" name="email" value="{{email}}" /></td>
     </tr>
     <tr>
-        <th scope="row"><label for="gnses_subject">Subject</label></th>
+        <th scope="row"><label for="gnses_subject">Subject:</label></th>
         <td><input type="text" class="regular-text" id="gnses_subject" name="subject" value="{{subject}}" /></td>
     </tr>
     <tr>
-        <th scope="row"><label for="gnses_message">Message</label></th>
+        <th scope="row"><label for="gnses_message">Message:</label></th>
         <td><textarea id="gnses_message" name="message" cols="80" rows="10">{{message}}</textarea></td>
     </tr>
 </tbody>
